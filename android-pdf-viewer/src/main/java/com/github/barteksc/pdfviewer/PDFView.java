59b10438181f4ecddaaeeae44d76cd7245fe0f8e
odmfl <77236251+odmfl@users.noreply.github.com>
2026-01-10 08:33:57 +0100

    Try

--- android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/PDFView.java
+++ android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/PDFView.java
@@ -1115,6 +1115,28 @@ public class PDFView extends RelativeLayout {
         canvas.translate(-currentXOffset, -currentYOffset);
     }
 
+    public class PdfFile {
+
+    private final SparseArray<String> pageText = new SparseArray<>();
+    private final SparseArray<RectF[]> pageTextRects = new SparseArray<>();
+
+    public void extractAllText(PdfiumCore core, PdfDocument doc) {
+        for (int i = 0; i < getPagesCount(); i++) {
+            pageText.put(i, core.getPageText(doc, i));
+            pageTextRects.put(i, core.getPageTextRects(doc, i));
+        }
+    }
+
+    public String getPageText(int page) {
+        return pageText.get(page);
+    }
+
+    public RectF[] getPageTextRects(int page) {
+        return pageTextRects.get(page);
+    }
+}
+
+
     public String getSelection() {
         if (selectionPaintView != null) {
             try {
@@ -2220,3 +2242,4 @@ public class PDFView extends RelativeLayout {
         void onSelection(boolean hasSelection);
     }
 }
+
